944356dd2746a04ff5af848c9de5a6783b7d307c
jyzhou95 <41101438+jyzhou95@users.noreply.github.com>
2019-04-08 17:40:37 -0400

    Added greedy alphas to test

--- generateAlpha.R
+++ generateAlpha.R
@@ -8,6 +8,7 @@ alpha_list <- list.files(paste0(parent_dir, "/alpha_list"))
 volatility_indicator_file_name <- paste0(parent_dir, "/indicator_list/volatility_signal.txt")
 volume_indicator_file_name <- paste0(parent_dir, "/indicator_list/volume_signal.txt")
 sentiment_indicator_file_name <- paste0(parent_dir, "/indicator_list/sentiment_signal.txt")
+ema_indicator_file_name <- paste0(parent_dir, "/indicator_list/ema_signal.txt")
 
 volatility_signal_code <- readChar(volatility_indicator_file_name, 
                                    file.info(volatility_indicator_file_name)$size)
@@ -17,6 +18,9 @@ volume_signal_code <- readChar(volume_indicator_file_name,
 
 sentiment_signal_code <- readChar(sentiment_indicator_file_name,
                                   file.info(sentiment_indicator_file_name)$size)
+
+ema_signal_code <- readChar(ema_indicator_file_name,
+                            file.info(ema_indicator_file_name)$size)
 
 dt.alpha_list <- data.table()
 
@@ -106,6 +110,44 @@ for (i in 1:(length(alpha_list)-1)){
     alpha_4_code_final <- paste0("a=", alpha_2_code, ";")
     
     my_alpha_final_2 <- paste0(alpha_3_code_final, alpha_4_code_final, sentiment_signal_code)
+    
+    dt.alpha_list <- rbind(dt.alpha_list,
+                           data.table(id=id+1,
+                                      code=my_alpha_final_2))
+    
+    id <- id + 2
+  }
+}
+
+# Get EMA signal
+for (i in 1:(length(alpha_list)-1)){
+  for (j in 2:length(alpha_list)){
+    alpha_1 <- paste0(parent_dir, "/alpha_list/", alpha_list[i])
+    alpha_2 <- paste0(parent_dir, "/alpha_list/", alpha_list[j])
+    
+    alpha_1_code <- trimws(readChar(alpha_1, 
+                                    file.info(alpha_1)$size), "both")
+    alpha_1_code_final <- paste0("a=", alpha_1_code, ";")
+    
+    alpha_2_code <- trimws(readChar(alpha_2, 
+                                    file.info(alpha_2)$size), "both")
+    alpha_2_code_final <- paste0("b=", alpha_2_code, ";")
+    
+    my_alpha_final <- paste0(alpha_1_code_final, alpha_2_code_final, ema_signal_code)
+    
+    dt.alpha_list <- rbind(dt.alpha_list,
+                           data.table(id=id,
+                                      code=my_alpha_final))
+    
+    alpha_3_code <- trimws(readChar(alpha_1, 
+                                    file.info(alpha_1)$size), "both")
+    alpha_3_code_final <- paste0("b=", alpha_3_code, ";")
+    
+    alpha_4_code <- trimws(readChar(alpha_2, 
+                                    file.info(alpha_2)$size), "both")
+    alpha_4_code_final <- paste0("a=", alpha_2_code, ";")
+    
+    my_alpha_final_2 <- paste0(alpha_3_code_final, alpha_4_code_final, ema_signal_code)
     
     dt.alpha_list <- rbind(dt.alpha_list,
                            data.table(id=id+1,
